a6639b7b94a97be067dc5fd1279d83d06cbc25e7
rozanashaqqoura <rozanashaqqou74@gmail.com>
2023-10-03 21:02:55 +0300

    fix

--- asstes/js/script.js
+++ asstes/js/script.js
@@ -4,24 +4,24 @@ let listBook = [
     {
         title :'الأنوار لا تتزاحم',
         author: "محمد أنوار",
-        imag : '/asstes/imag/Books_IT688.webp'
+        imag : 'asstes/imag/Books_IT688.webp'
     },
     {
         title :'محمد سعيد',
         author: "محمد سعيد",
-        imag : '/asstes/imag/سحر-الرياضيات-آرثر-بنيامين_optimize-1.webp'
+        imag : 'asstes/imag/سحر-الرياضيات-آرثر-بنيامين_optimize-1.webp'
     },  {
         title :'سحر الرياضيات',
         author: "محمد أنوار",
-        imag : '/asstes/imag/4416_محمد_التهامي_حراق.jpg'
+        imag : 'asstes/imag/4416_محمد_التهامي_حراق.jpg'
     },  {
         title :'زمن علم',
         author: "مشيل هنري",
-        imag : '/asstes/imag/الهمجية-؛-زمن-علم-بلا-ثقافة-ميشيل-هنري-موقع-المكتبة-نت-www.maktbah.net-4.webp'
+        imag : 'asstes/imag/الهمجية-؛-زمن-علم-بلا-ثقافة-ميشيل-هنري-موقع-المكتبة-نت-www.maktbah.net-4.webp'
     },  {
         title :'مكتبه  نوبل',
         author: "علي نوبل",
-        imag :'/asstes/imag/20191217232342396100.jpg'
+        imag :'asstes/imag/20191217232342396100.jpg'
 
     }
 ]
